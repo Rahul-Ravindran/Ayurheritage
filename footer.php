48f5a058c6c4047adc72a9a109cf0bb59b34246c
Rahul-Ravindran <kannan6611@gmail.com>
2022-07-08 00:03:17 +0530

    project directory structure created

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+  </div>
+  <!-- Body container ends here -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <title>Ayur Heritage</title>
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" type="images/ico" href="images/favicon.svg">
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div id="body-container">
